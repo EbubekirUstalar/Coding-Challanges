36d864de5d54b72794df81b4ec1a6eb6f409cd76
Ebubekir Ustalar <ebubekirustalar@gmail.com>
2019-06-26 16:24:40 +0300

    Update VowelFamilies.py

--- Vowel Families/VowelFamilies.py
+++ Vowel Families/VowelFamilies.py
@@ -1,21 +1,21 @@
 
 def same_vowel_group(w):
-    vowels = ['a', 'e', 'i', 'u', 'o']
+    vowels = ['a', 'e', 'i', 'u', 'o']                                ## Creating vowels
 
-    hashtable = {}
+    hashtable = {}                                                    ## making a hashtable to hold evey word inside of them will be list of vowels
     for i, word in enumerate(w):
-        if(word not in hashtable.keys()):
+        if(word not in hashtable.keys()):                             ## non repeating words
             hashtable[word] = hashtable.setdefault(word, [])
             for char in word:
                 if(char in vowels and char not in hashtable[word]):
                     hashtable[word].append(char)
 
-    selectedWords = []
+    selectedWords = []                                                ## the output we want
     block = False
     for i, word in enumerate(hashtable):
         for j, otherword in enumerate(hashtable):
             block = False
-            if(j == i):
+            if(j == i):                                               ## we dont want the same index to match
                 block = True
                 continue
             else:
@@ -25,7 +25,7 @@ def same_vowel_group(w):
                             block = True
                             break
             if(block == False and len(hashtable[word]) == len(hashtable[otherword])):
-                selectedWords.append(word)
+                selectedWords.append(word)                           ## choosing the selected indexes
                 break
 
     if(len(selectedWords) == 0):
@@ -33,10 +33,10 @@ def same_vowel_group(w):
 
     print(selectedWords)
     return sorted(selectedWords, reverse = True)
-
-w = ['bot', 'bottom', 'hoops']
-
-print(same_vowel_group(w))
+ 
+w = ['bot', 'bottom', 'hoops']                                       ## test list
+            
+print(same_vowel_group(w))                                           ## output
 
 
 
