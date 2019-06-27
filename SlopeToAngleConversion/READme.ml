bb20d8ea5eaf179fbf2ed24d14bc87c0fd701cdc
Ebubekir Ustalar <ebubekirustalar@gmail.com>
2019-06-27 15:00:55 +0300

    Create READme.ml

---
+++ SlopeToAngleConversion/READme.ml
@@ -0,0 +1,31 @@
+
+I did my second challenge on https://edabit.com/challenge/jowQ2aeZut4vGyHyP
+
+
+
+
+Description
+
+Slope to Angle Conversion
+
+Published by: https://edabit.com/user/7MRr7qZpuXEo66ZoR
+
+Key concepts: algebra, math, number
+
+Language: Python
+
+Given a slope of a line, calculate and return the value of the angle of line (relative to the y -axis). For example, a horizontal line would be 90 degrees.
+
+convert(1) ➞ 45
+
+convert(0) ➞ 90
+
+convert(-1) ➞ 135
+
+
+
+Notes
+-All values returned should be in degrees.
+-All values returned should be rounded to the nearest whole number.
+-The value to return must be strictly between 0 and 180.
+-All inputs will be valid integer values.
