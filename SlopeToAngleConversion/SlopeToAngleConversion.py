32a9b00c499f311b07f4e4f352be25320b024957
Ebubekir Ustalar <ebubekirustalar@gmail.com>
2019-06-27 14:54:37 +0300

    Update SlopeToAngleConversion.py

--- SlopeToAngleConversion/SlopeToAngleConversion.py
+++ SlopeToAngleConversion/SlopeToAngleConversion.py
@@ -1,20 +1,16 @@
-# def arctan(slope):
-#     ## a simple line equation is like: y = ax + b
-#     ## slope can be found by taking derivative of y
-#     ## in this case we could safely say the slope is equal to coefficient of a
-    
-#     ## arctan(x) = 2arctan(x/(1+(1+x^2)^1/2))
-    
 
-#     if(slope < 0.00001):
-#         return slope
-#     else:
-#         newSlope = 0.0
-#         newSlope = slope / (1 + ((1 + slope**(2))**(1/2)))
-#         return 2 * arctan(newSlope)
+## a simple line equation is like: y = ax + b
+## slope can be found by taking derivative of y
+## in this case we could safely say the slope is equal to coefficient of a
+
 
 def convert(slope):
-    arctan = lambda x: 2 * arctan(x / (1 + ((1 + x**(2))**(1/2)))) if abs(x) > 0.0001 else x
-    return 90-round(arctan(slope) * 180 /  3.14159265359)
+    arctan = lambda x: 2 * arctan(x / (1 + ((1 + x**(2))**(1/2)))) if abs(x) > 0.0001 else x ## arctan(x) = 2arctan(x/(1+(1+x^2)^1/2))
+                                                                                             ## In small value of x we can say that arctan(x) = x, So I wanted to lower degree over time
+    return 90-round(arctan(slope) * 180 /  3.14159265359)                                    ## The result return radian we need to change it to degree
 
 print(convert(-20))
+
+
+## I wanted to write it in a more less code way, but i did not know that we can import libraries
+## everybody besides me imported library
